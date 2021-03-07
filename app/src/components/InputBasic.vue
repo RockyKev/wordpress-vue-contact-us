d1296aca0b3f8c66b4d8108b73f8c777bebb9c04
rockykev <kev.rocky@gmail.com>
2021-03-07 13:54:14 -0800

    working multi-root components

---
+++ app/src/components/InputBasic.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="form-input">
+  <label v-if="label">{{ label }}</label>
+  <input
+    v-bind="$attrs"
+    :value="modelValue"
+    :placeholder="placeholder"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="field"
+  >
+
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    placeholder: {
+        type: [String],
+        default: ''
+    }
+  }
+}
+</script>
--- app/src/components/InputText.vue
+++ app/src/components/InputText.vue
@@ -1,7 +1,62 @@
 <template>
+  <div class="form-input">
+    <label :for="inputID"><slot></slot><span v-show="required" class="red">*</span></label>
+
+      <input
+        v-bind="$attrs"
+        :id="inputID"
+        :value="modelValue"
+        :placeholder="placeholder"
+        :class="className"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+  </div>
 
 </template>
 
 <script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    placeholder: {
+      type: [String],
+      default: "",
+    },
+    className: {
+      type: [String],
+      default: "",
+    },
+    required: {
+      type: [Boolean],
+      default: false,
+    },
+    inputID: {
+        type: [String],
+        default: "1"
+    }
+  },
+};
 
+// export default {
+//     props: ['dataName', 'placeholder', 'required'],
+//     data() {
+//         return {
+//             content: this.value
+
+//         }
+//     },
+//     methods: {
+//         handleInput (e) {
+//             console.log("handling!", e);
+//             this.$emit('input', this.content);
+//         }
+//     }
+// }
 </script>
